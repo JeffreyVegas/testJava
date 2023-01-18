d6a6e4bda97088853b8a9310f73f8707b9c262f3
Jeffrey <jeffreyvegas.web@gmail.com>
2023-01-18 10:21:19 -0500

    now. this is the second

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is a earth radio, and now humanity music");
+        System.out.println("this is a new line, i don't what i'm doing");
     }
 }
