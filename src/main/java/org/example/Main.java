b1f84dc17210b714713ad437d98c178e27a5a26c
Jeffrey <jeffreyvegas.web@gmail.com>
2023-01-18 12:02:15 -0500

    i don't understand :(

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
         System.out.println("this is a earth radio, and now humanity music");
         System.out.println("this is a new line, i don't what i'm doiun");
         System.out.println("now, we are in the second branch");
         System.out.println("now from github page jijijiji");
+        System.out.println("WTF BRO?");
     }
 }
