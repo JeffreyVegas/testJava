ea96b097a91aff60f60c993eaed0d03d0ee26a39
Jeffrey <jeffreyvegas.web@gmail.com>
2023-01-18 11:29:19 -0500

    hi, from other dimension

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("this is a earth radio, and now humanity music");
-        System.out.println("this is a new line, i don't what i'm doing");
+        System.out.println("this is a new line, i don't what i'm doiun");
+        System.out.println("now, we are in the second branch");
     }
 }
